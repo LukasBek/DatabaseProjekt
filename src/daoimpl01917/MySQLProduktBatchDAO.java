96a987c8725ab92505016b73c4beb1a6afb0a766
Big Fat Llama <lukas@10.16.99.208>
2016-03-10 12:33:45 +0100

    opdateret

--- src/daoimpl01917/MySQLProduktBatchDAO.java
+++ src/daoimpl01917/MySQLProduktBatchDAO.java
@@ -1,5 +1,7 @@
 package daoimpl01917;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import connector01917.Connector;
@@ -11,8 +13,14 @@ public class MySQLProduktBatchDAO implements ProduktBatchDAO {
 
 	@Override
 	public ProduktBatchDTO getProduktBatch(int pbId) throws DALException {
-//		ResultSet rs = Connector.doQuery("SELECT * ")
-		return null;
+		ResultSet pb = Connector.doQuery("SELECT * FROM produktbatch WHERE pb_id = "+pbId);
+		try{
+			if (!pb.first()){
+				throw new DALException("Produkt batch nonexistent");
+			}
+			return new ProduktBatchDTO (pb.getInt("pb_id"), pb.getInt("status"), pb.getInt("recept_id"));
+		}catch(SQLException e) {throw new DALException(e);}
+		
 	}
 
 	@Override
