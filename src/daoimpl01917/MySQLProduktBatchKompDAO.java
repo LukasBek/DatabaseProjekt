c0a77afbf153d08ed2a249011da78111dfd01ff1
SebbyG <seb.hop@hotmail.com>
2016-03-15 10:24:05 +0100

    Rettet klasse i daoimpl01917

--- src/daoimpl01917/MySQLProduktBatchKompDAO.java
+++ src/daoimpl01917/MySQLProduktBatchKompDAO.java
@@ -2,7 +2,7 @@ package daoimpl01917;
 
 import dto01917.ProduktBatchKompDTO;
 
-import java.awt.List;
+import java.util.List;
 
 import daointerfaces01917.DALException;
 import daointerfaces01917.ProduktBatchKompDAO;
@@ -37,18 +37,6 @@ public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO{
 	public void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
 		// TODO Auto-generated method stub
 		
-	}
-
-	@Override
-	public java.util.List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public java.util.List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
