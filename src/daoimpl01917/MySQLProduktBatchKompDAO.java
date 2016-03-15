593102e3e4e6a071af36dbad70b63f17e8096205
Quaade94 <larsquaadeconnect@gmail.com>
2016-03-15 10:01:13 +0100

    Implementere MysSQLProduktBatchKomp

---
+++ src/daoimpl01917/MySQLProduktBatchKompDAO.java
@@ -0,0 +1,54 @@
+package daoimpl01917;
+
+import dto01917.ProduktBatchKompDTO;
+
+import java.awt.List;
+
+import daointerfaces01917.DALException;
+import daointerfaces01917.ProduktBatchKompDAO;
+
+public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO{
+
+	@Override
+	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public java.util.List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public java.util.List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
